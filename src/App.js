10eb374d7f1b69425efd1da63192f5489c203e4c
unknown <saidi.firas@outlook.com>
2022-11-22 20:59:38 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -1,42 +1,23 @@
 import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Login from './components/Login'
 import SignUp from './components/Signup'
 import UserDetails from "./components/UserDetails";
+import Header from './components/Header'
 
 function App() {
   return (
-    <Router>
+    <Router basename={'/log-app'}>
       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          
-          <div className="container">
-            <Link className="navbar-brand" to={'/sign-in'}>
-              Home
-            </Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-in'}>
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-up'}>
-                    Sign up
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        
+        <Header/>
 
         <div className="auth-wrapper">
           <div className="auth-inner">
-            <Routes basename={'/log-app'}>
+            <Routes >
               <Route exact path="/" element={<Login />} />
               <Route path="/sign-in" element={<Login />} />
               <Route path="/sign-up" element={<SignUp />} />
